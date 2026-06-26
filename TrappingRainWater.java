02bb34d458f1e70fa87bc6f768fff452171a211e
LOKESHWARAAN K <25cs116@rmkcet.ac.in>
2026-06-26 11:58:14 +0530

    Add TrappingRainWater.java starter

---
+++ TrappingRainWater.java
@@ -0,0 +1,11 @@
+import java.io.*;
+
+public class TrappingRainWater {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        // TODO: Read the required input for this problem.
+        // TODO: Implement Lokeshwaran K's own solution logic here.
+        // TODO: Print the expected output format.
+    }
+}
